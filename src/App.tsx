75a0a05e12738bd683fe5e67a4bf7d83ad0b7a3b
Eduardo Sotero <eduardo@redpanda.events>
2020-05-08 17:34:44 -0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const { REACT_APP_TEXT_EXAMPLE } = process.env;
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {REACT_APP_TEXT_EXAMPLE} <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
