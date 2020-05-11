642718459c9de6fec6407050ea17858f2bd71c93
Eduardo Sotero <eduardo@redpanda.events>
2020-05-11 18:12:39 -0300

    Some setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,9 @@ import React from "react";
 import "./App.css";
 
 const { REACT_APP_TWITCH_CLIENT_ID, REACT_APP_TWITCH_USERNAME } = process.env;
-const twitchClientId: string = REACT_APP_TWITCH_CLIENT_ID || "";
 const twitchUserName: string = REACT_APP_TWITCH_USERNAME || "";
 
 function App() {
-  const [isLive, setIsLive] = React.useState(false);
-  const processTwitchStreamCallback = React.useCallback(
-    processTwitchStream,
-    []
-  );
-
-  function fetchTwitchData() {
-    fetch(`https://api.twitch.tv/helix/streams?user_login=${twitchUserName}`, {
-      headers: {
-        "Client-ID": twitchClientId || "",
-      },
-    })
-      .then(async (res) => {
-        const response = await res.json();
-        if (Boolean(response.data && response.data[0])) {
-          setIsLive(true);
-        }
-      })
-      .catch((err) => {
-        console.log("Error fetching data from Twitch API: ", err);
-      });
-  }
-
-  function processTwitchStream() {
-    if (twitchClientId && twitchUserName) {
-      fetchTwitchData();
-    } else {
-      console.error(
-        "[react-livestream] Twitch support requires a twitchClientId and twitchUserName prop"
-      );
-    }
-  }
-
   function TwitchIframe() {
     return (
       <div className="iframe-wrapper">
@@ -48,14 +14,6 @@ function App() {
           src={`https://player.twitch.tv/?channel=${twitchUserName}`}
           frameBorder="0"
         />
-      </div>
-    );
-  }
-
-  function OfflineComponent() {
-    return (
-      <div className="offline-wrapper">
-        <div className="offline-frame">Currently OFFLINE</div>
       </div>
     );
   }
@@ -74,13 +32,9 @@ function App() {
     );
   }
 
-  React.useEffect(() => {
-    processTwitchStreamCallback();
-  }, [processTwitchStreamCallback]);
-
   return (
     <div className="App">
-      {isLive ? <TwitchIframe /> : <OfflineComponent />}
+      <TwitchIframe />
       <ControlDeck />
     </div>
   );
